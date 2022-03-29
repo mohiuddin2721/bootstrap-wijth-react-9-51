c766ec38240fa080d56bf945bc49c2e0b0023f6b
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-03-29 15:27:34 +0600

    general bootstrap are deleted and react bootstrap npm are installed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import CardGroup from './Components/CardGroup/CardGroup';
-import Button from './Components/Button/Button';
 
 function App() {
   return (
     <div className="App">
-      <Button></Button>
-      <CardGroup></CardGroup>
+      
     </div>
   );
 }
